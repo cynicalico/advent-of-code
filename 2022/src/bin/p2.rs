a9e680119617b8039797942a70bf323395ea2cd6
beauregardes <93552084+beauregardes@users.noreply.github.com>
2022-12-06 16:49:48 -0700

    fixed type

--- 2022/src/bin/p2.rs
+++ 2022/src/bin/p2.rs
@@ -5,7 +5,7 @@ fn main() {
     // let base_points = HashMap::from([("X", 1), ("Y", 2), ("Z", 3)]);
     let rock_points = HashMap::from([("A", 3), ("B", 0), ("C", 6)]);
     let paper_points = HashMap::from([("A", 6), ("B", 3), ("C", 0)]);
-    let scissors_poinst = HashMap::from([("A", 0), ("B", 6), ("C", 3)]);
+    let scissors_points = HashMap::from([("A", 0), ("B", 6), ("C", 3)]);
 
     let Ok(lines) = read_lines("res/p2.txt") else { return };
     let p1_ans = lines
@@ -17,7 +17,7 @@ fn main() {
                 match *you {
                     "X" => tot + 1 + rock_points[opponent],
                     "Y" => tot + 2 + paper_points[opponent],
-                    "Z" => tot + 3 + scissors_poinst[opponent],
+                    "Z" => tot + 3 + scissors_points[opponent],
                     _ => unreachable!()
                 }
             } else {
