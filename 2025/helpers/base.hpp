d652ecffee189af33a86c5bf76dcd708665bd48f
cynicalico <228817350+cynicalico@users.noreply.github.com>
2025-12-01 21:10:40 -0700

    unit

--- 2025/helpers/base.hpp
+++ 2025/helpers/base.hpp
@@ -13,26 +13,26 @@
     p2_ans_t p2(const parse_result_t &input);                       \
     }
 
-#define SOLUTION_MAIN(ns)                                                                         \
-    int main(int, char *[]) {                                                                     \
-        using namespace std::chrono;                                                              \
-        using namespace ns;                                                                       \
-                                                                                                  \
-        auto start = steady_clock::now();                                                         \
-        try {                                                                                     \
-            const auto input = parse_input_file();                                                \
-            fmt::print(#ns "::p1: {}\n", p1(input));                                              \
-            fmt::print(#ns "::p2: {}\n", p2(input));                                              \
-        } catch (const std::runtime_error &e) {                                                   \
-            fmt::print("ERROR: {} \n", e.what());                                                 \
-        }                                                                                         \
-        auto end = steady_clock::now();                                                           \
-                                                                                                  \
-        const auto elapsed = end - start;                                                         \
-        const auto elapsed_ns = duration_cast<nanoseconds>(elapsed);                              \
-        fmt::print("\nElapsed time: {:.05f}ms\n", static_cast<double>(elapsed_ns.count()) / 1e9); \
-                                                                                                  \
-        return 0;                                                                                 \
+#define SOLUTION_MAIN(ns)                                                                        \
+    int main(int, char *[]) {                                                                    \
+        using namespace std::chrono;                                                             \
+        using namespace ns;                                                                      \
+                                                                                                 \
+        auto start = steady_clock::now();                                                        \
+        try {                                                                                    \
+            const auto input = parse_input_file();                                               \
+            fmt::print(#ns "::p1: {}\n", p1(input));                                             \
+            fmt::print(#ns "::p2: {}\n", p2(input));                                             \
+        } catch (const std::runtime_error &e) {                                                  \
+            fmt::print("ERROR: {} \n", e.what());                                                \
+        }                                                                                        \
+        auto end = steady_clock::now();                                                          \
+                                                                                                 \
+        const auto elapsed = end - start;                                                        \
+        const auto elapsed_ns = duration_cast<nanoseconds>(elapsed);                             \
+        fmt::print("\nElapsed time: {:.06f}s\n", static_cast<double>(elapsed_ns.count()) / 1e9); \
+                                                                                                 \
+        return 0;                                                                                \
     }
 
 #define SOLUTION(ns, parse_result_t, p1_ans_t, p2_ans_t)        \
