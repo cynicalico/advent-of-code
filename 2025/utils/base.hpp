474e8d76abb09286ca170bee19b96437e66a3533
cynicalico <228817350+cynicalico@users.noreply.github.com>
2025-12-02 20:11:06 -0700

    don't include i/o timings

--- 2025/utils/base.hpp
+++ 2025/utils/base.hpp
@@ -19,16 +19,19 @@
         UNPACK p2_ans_t p2(const UNPACK parse_result_t &input);           \
     }
 
-#define SOLUTION_MAIN(yyyy, dd)                                                                  \
+#define SOLUTION_MAIN(yyyy, dd, p1_ans_t, p2_ans_t)                                              \
     int main(int, char *[]) {                                                                    \
         using namespace std::chrono;                                                             \
         using namespace aoc##yyyy::day##dd;                                                      \
                                                                                                  \
+        auto p1_ans = UNPACK p1_ans_t();                                                         \
+        auto p2_ans = UNPACK p2_ans_t();                                                         \
+                                                                                                 \
         auto start = steady_clock::now();                                                        \
         try {                                                                                    \
             const auto input = parse_input_file();                                               \
-            fmt::print(STRINGIZE(aoc## yyyy) "::" STRINGIZE(day## dd) "::p1: {}\n", p1(input));    \
-            fmt::print(STRINGIZE(aoc## yyyy) "::" STRINGIZE(day## dd) "::p2: {}\n", p2(input));    \
+            p1_ans = p1(input);                                                                  \
+            p2_ans = p2(input);                                                                  \
         } catch (const std::runtime_error &e) {                                                  \
             fmt::print("ERROR: {} \n", e.what());                                                \
         }                                                                                        \
@@ -36,6 +39,9 @@
                                                                                                  \
         const auto elapsed = end - start;                                                        \
         const auto elapsed_ns = duration_cast<nanoseconds>(elapsed);                             \
+                                                                                                 \
+        fmt::print(STRINGIZE(aoc## yyyy) "::" STRINGIZE(day## dd) "::p1: {}\n", p1_ans);           \
+        fmt::print(STRINGIZE(aoc## yyyy) "::" STRINGIZE(day## dd) "::p2: {}\n", p2_ans);           \
         fmt::print("\nElapsed time: {:.06f}s\n", static_cast<double>(elapsed_ns.count()) / 1e9); \
                                                                                                  \
         return 0;                                                                                \
@@ -43,4 +49,4 @@
 
 #define SOLUTION(yyyy, dd, parse_result_t, p1_ans_t, p2_ans_t)        \
     SOLUTION_PROTOTYPES(yyyy, dd, parse_result_t, p1_ans_t, p2_ans_t) \
-    SOLUTION_MAIN(yyyy, dd)
+    SOLUTION_MAIN(yyyy, dd, p1_ans_t, p2_ans_t)
