fb710d5b67d68852d53b7f85966e86a160f22401
cynicalico <228817350+cynicalico@users.noreply.github.com>
2025-12-10 14:50:02 -0700

    clean up

--- solutions/2025/day09.cpp
+++ solutions/2025/day09.cpp
@@ -24,6 +24,14 @@ std::int64_t find_largest_rectangle(const std::vector<Pos> &input) {
             max = std::max(max, (std::abs(input[j].x - input[i].x) + 1) * (std::abs(input[j].y - input[i].y) + 1));
     return max;
 }
+}
+
+std::int64_t AOC_NS(2025, 9)::p1(std::vector<Pos> &input) { return find_largest_rectangle(input); }
+
+namespace AOC_NS(2025, 9) {
+std::tuple<std::int64_t, std::int64_t, std::int64_t, std::int64_t> lrtb(const Pos &p1, const Pos &p2) {
+    return {std::min(p1.x, p2.x), std::max(p1.x, p2.x), std::min(p1.y, p2.y), std::max(p1.y, p2.y)};
+}
 
 bool check_corners(const Grid &grid, const Pos &c1, const Pos &c2) {
     const auto c3 = Pos(c1.x, c2.y);
@@ -32,18 +40,11 @@ bool check_corners(const Grid &grid, const Pos &c1, const Pos &c2) {
 }
 
 bool all_red_or_green_edges(const Grid &grid, const Pos &c1, const Pos &c2) {
-    const auto sx = std::min(c1.x, c2.x);
-    const auto ex = std::max(c1.x, c2.x);
-    const auto sy = std::min(c1.y, c2.y);
-    const auto ey = std::max(c1.y, c2.y);
-    for (std::size_t x = sx; x <= ex; ++x) {
-        if (grid[Pos(x, sy)] == 0) return false;
-        if (grid[Pos(x, ey)] == 0) return false;
-    }
-    for (std::size_t y = sy; y <= ey; ++y) {
-        if (grid[Pos(sx, y)] == 0) return false;
-        if (grid[Pos(ex, y)] == 0) return false;
-    }
+    const auto [l, r, t, b] = lrtb(c1, c2);
+    for (std::size_t x = l; x <= r; ++x)
+        if (grid[Pos(x, t)] == 0 || grid[Pos(x, b)] == 0) return false;
+    for (std::size_t y = t; y <= b; ++y)
+        if (grid[Pos(l, y)] == 0 || grid[Pos(r, y)] == 0) return false;
     return true;
 }
 
@@ -52,8 +53,7 @@ std::int64_t find_largest_rectangle_colored(const Grid &grid, const std::vector<
     for (std::size_t i = 0; i < input.size(); ++i) {
         for (std::size_t j = i + 1; j < input.size(); ++j) {
             const auto area = (std::abs(input[j].x - input[i].x) + 1) * (std::abs(input[j].y - input[i].y) + 1);
-            if (area < max) continue;
-            if (!check_corners(grid, input[i], input[j])) continue;
+            if (area < max || !check_corners(grid, input[i], input[j])) continue;
             if (all_red_or_green_edges(grid, input[i], input[j])) max = area;
         }
     }
@@ -61,26 +61,22 @@ std::int64_t find_largest_rectangle_colored(const Grid &grid, const std::vector<
 }
 }
 
-std::int64_t AOC_NS(2025, 9)::p1(std::vector<Pos> &input) { return find_largest_rectangle(input); }
-
 std::int64_t AOC_NS(2025, 9)::p2(std::vector<Pos> &input) {
-    const auto max_x = std::ranges::max(input, [](const auto &a, const auto &b) { return a.x < b.x; }).x;
-    const auto max_y = std::ranges::max(input, [](const auto &a, const auto &b) { return a.y < b.y; }).y;
-    auto grid = Grid(max_x + 1, max_y + 1);
+    auto grid = Grid(std::ranges::max(input, [](const auto &a, const auto &b) { return a.x < b.x; }).x + 1,
+                     std::ranges::max(input, [](const auto &a, const auto &b) { return a.y < b.y; }).y + 1);
 
+    // Populate the red tiles + edges between them
     input.emplace_back(input[0]);
     for (const auto &[from, to] : input | std::views::adjacent<2>) {
-        const auto sx = std::min(from.x, to.x);
-        const auto ex = std::max(from.x, to.x);
-        const auto sy = std::min(from.y, to.y);
-        const auto ey = std::max(from.y, to.y);
-        for (std::size_t y = sy; y <= ey; ++y)
-            for (std::size_t x = sx; x <= ex; ++x) grid[Pos(x, y)] = 'X';
+        const auto [l, r, t, b] = lrtb(from, to);
+        for (std::size_t y = t; y <= b; ++y)
+            for (std::size_t x = l; x <= r; ++x) grid[Pos(x, y)] = 'X';
         grid[from] = '#';
         grid[to] = '#';
     }
     input.pop_back();
 
+    // Fill the interior of the shape with green tiles
     for (std::size_t y = 0; y < grid.height(); ++y) {
         bool inside = false;
         for (std::size_t x = 0; x < grid.width(); ++x) {
